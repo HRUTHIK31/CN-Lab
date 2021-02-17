c7b749b1418746cb9decb00f27e143e98a0c62e5
Hruthik Gurugubelli <hruthikgurugubelli123@gmail.com>
2021-02-18 00:19:01 +0530

    Add files via upload

---
+++ LAB9/run.sh
@@ -0,0 +1,5 @@
+gnome-terminal -- ./a.out neighbors1.txt vectors.txt 1 18181 2 &
+gnome-terminal -- ./a.out neighbors2.txt vectors.txt 2 18182 3 &
+gnome-terminal -- ./a.out neighbors3.txt vectors.txt 3 18183 2 &
+gnome-terminal -- ./a.out neighbors4.txt vectors.txt 4 18184 3 &
+gnome-terminal -- ./a.out neighbors5.txt vectors.txt 5 18185 2
